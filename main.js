672f9a83056d7f9710a6003e6e4b82930d9110af
shivani gupta <shivanigupta4673@gmail.com>
2022-06-29 19:55:32 +0530

    upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//Get All Values
+const inputTxt = document.querySelector('.inputTxt');
+var button = document.querySelector('.btn');
+const showData = document.querySelector('.showData');
+
+//Get API Key By Login To OpenWeather.org
+const API_Key = "8ae071b593ee1958f22d713a799e19a6";
+
+//Now Add Event Listener
+button.addEventListener('click', () => {
+
+    //Get Input Value
+    const cityInput = inputTxt.value;
+    // console.log(cityInput);
+
+    //Now Fetch Through Get API 
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+
+            //When Fill Input Field Then Clear Input Field
+            inputTxt.value = " ";
+
+            //Now Show All Data Value
+            showData.innerHTML = `
+                                <ul>
+                                    <li class="desc">${data.weather[0].description}</li>
+                                    <li class="city">${data.name}</li>
+                                    <li class="temp">${data.main.temp}°c</li>
+                                </ul>
+                                `; 
+
+        });
+
+});
+
